e8912bfb1c8a1109db415742a198e02066eb5a49
DiegoMull <anlumull@gmail.com>
2022-11-04 02:49:51 -0600

    Creacion de los metodos get, post, put y delete para pruebas

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ import express from "express"
 
 const app = express()
 
+app.get('/recipes', (req, res) => res.send('obteniendo las recetas'))
+app.post('/recipes', (req, res) => res.send('creando las recetas'))
+app.put('/recipes', (req, res) => res.send('actualizando las recetas'))
+app.delete('/recipes', (req, res) => res.send('borando las recetas'))
+
 app.listen(3000)
 console.log("server running on port 3000")
