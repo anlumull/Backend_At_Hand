1869af70cc2d7fd0e9c695feb555e8faf8b98e8d
DiegoMull <anlumull@gmail.com>
2022-11-04 03:59:20 -0600

    Creada la conexion a la base de datos en Atlas mongodb y ordenamiento del codigo para mayor entendimiento, ademas de la importacion del modulo mongoose y dotenv para el manejo de variables de entorno

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express"
+import {connectDB} from './DB/db.js'
+import {PORT} from './config.js';
 
 const app = express()
+connectDB()
+
 
 app.get('/recipes', (req, res) => res.send('obteniendo las recetas'))
 app.post('/recipes', (req, res) => res.send('creando las recetas'))
 app.put('/recipes', (req, res) => res.send('actualizando las recetas'))
 app.delete('/recipes', (req, res) => res.send('borando las recetas'))
 
-app.listen(3000)
-console.log("server running on port 3000")
+app.listen(PORT)
+console.log("server running on port ", PORT)
