357eceb98fa15c66856dd7aac9931964650219f2
DiegoMull <anlumull@gmail.com>
2022-11-05 08:37:30 -0600

    Manejo de errores en el controlador de ingredientes

--- controllers/ingredient.controllers.js
+++ controllers/ingredient.controllers.js
@@ -1,34 +1,64 @@
-import Ingredient from '../models/ingredient.js'
+import Ingredient from "../models/ingredient.js";
 
 export const getIngredients = async (req, res) => {
-    const ingredient = await Ingredient.find()
-    res.send(ingredient)
-}
+  try {
+    throw new Error("error de prueba");
+    const ingredient = await Ingredient.find();
+    res.send(ingredient);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const postIngredient = async (req, res) => {
-    const {idIngrediente, nombre, details} = req.body
+  try {
+    const { idIngrediente, nombre, details } = req.body;
 
-    const newIngredient = new Ingredient({idIngrediente, nombre, details}) 
+    const newIngredient = new Ingredient({ idIngrediente, nombre, details });
 
-    await newIngredient.save()
+    await newIngredient.save();
 
-    return res.json(newIngredient)
-}
+    return res.json(newIngredient);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const putIngredient = async (req, res) => {
-    const ingrediente = await Ingredient.findByIdAndUpdate(req.params.id, req.body, {new : true}) 
-    console.log(ingrediente)
-    return res.send('recibido')
-}
+  try {
+    const ingrediente = await Ingredient.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    console.log(ingrediente);
+    return res.send(ingrediente);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const deleteIngredient = async (req, res) => {
-    const ingredientRemoved = await Ingredient.findByIdAndDelete(req.params.id)
-    if (!ingredientRemoved) return res.sendStatus(404)
-    return res.sendStatus(204)
-}
+  try {
+    const ingredientRemoved = await Ingredient.findByIdAndDelete(req.params.id);
+    if (!ingredientRemoved) return res.sendStatus(404);
+    return res.sendStatus(204);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const getIngredient = async (req, res) => {
-    const ingredient = await Ingredient.findById(req.params.id)
-    if(!ingredient) return res.sendStatus(404)
-    return res.json(ingredient)
-}
+  try {
+    const ingredient = await Ingredient.findById(req.params.id);
+    if (!ingredient) return res.sendStatus(404);
+    return res.json(ingredient);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
