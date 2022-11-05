94bbe6f6105e47076defbd3f586265ccebaac91c
DiegoMull <anlumull@gmail.com>
2022-11-05 08:43:07 -0600

    importacion del modulo express fileupload y implementacion en el directorio app.js para su utilizacion, ademas se creo un directorio para el guardado de archivos de prueba y el uso de archivos temporales

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express"
+import fileupload from 'express-fileupload';
 import recipeRoutes from './routes/recipe.routes.js'
 import ingredientRoutes from './routes/ingredient.routes.js'
 
 //middlewares
 const app = express()
 app.use(express.json())
-
+app.use(fileupload({
+    useTempFiles : true,
+    tempFileDir: "./upload"
+}))
 //Routes 
 app.use(recipeRoutes)
 app.use(ingredientRoutes) 
